d841e4a7bd7509f845fd476835d991767b44a575
Benjamin Crisman <bencrisman@bcsquared.tech>
2022-12-06 22:32:58 -0700

    parsing crates

--- day5/main.go
+++ day5/main.go
@@ -13,7 +13,14 @@ func main() {
 		log.Fatal(err)
 	}
 
-	println(len(lines))
+	crates, err := parseCrates(lines[0:3], 3)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, crate := range crates {
+		println(crate)
+	}
 }
 
 func getInput(path string) ([]string, error) {
@@ -25,8 +32,38 @@ func getInput(path string) ([]string, error) {
 	return strings.Split(string(f), "\r\n"), err
 }
 
+// source: https://stackoverflow.com/questions/28058278/how-do-i-reverse-a-slice-in-go
 func ReverseSlice[T comparable](s []T) {
 	sort.SliceStable(s, func(i, j int) bool {
 		return i > j
 	})
+}
+
+func parseCrates(lines []string, numCols int) ([]string, error) {
+	if len(lines) == 0 {
+		return nil, nil
+	}
+
+	// start from bottom
+	ReverseSlice(lines)
+	crates := make([]string, numCols)
+
+	for _, line := range lines {
+		col := 0
+
+		for i := 0; i < len(line); i += 3 {
+			i++
+
+			if string(line[i]) == " " {
+				col++
+				continue
+			}
+
+			crate := string(line[i])
+			crates[col] += crate
+			col++
+		}
+	}
+
+	return crates, nil
 }
